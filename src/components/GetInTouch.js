d8933dbb4b17029b4636aa5bc8fd25c85210a4da
Raul Martinez <raulmartinez@Rauls-MacBook-Pro.local>
2019-01-31 05:53:30 -0500

    added bootstrap and changed form

--- src/components/GetInTouch.js
+++ src/components/GetInTouch.js
@@ -9,15 +9,67 @@ export default function GetInTouch() {
       <h3>
         GET IN TOUCH <span>to plan yours</span>
       </h3>
-      <form action="">
-        <label htmlFor="name">Your name:</label>
-        <input type="text" name="name" id="name" />
-        <label htmlFor="phoneNumber">Your phone number:</label>
-        <input type="phone" name="phoneNumber" id="phoneNumber" />
-        <label htmlFor="emailAddress">Your email address:</label>
-        <input type="email" name="emailAddress" id="emailAddress" />
-        <textarea name="message" id="message" cols="30" rows="10" />
-      </form>
+      <div className="container">
+        <form
+          netlify
+          name="contact"
+          className="netlifyForm mb-3 mt-5"
+          method="post"
+        >
+          <div className="row">
+            <div className="col">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Your Name"
+                name="contactName"
+                id="contactName"
+                required
+              />
+            </div>
+            <div className="col">
+              <input
+                type="phone"
+                className="form-control"
+                placeholder="Your phone number"
+                name="phoneNumber"
+                id="phoneNumber"
+                required
+              />
+            </div>
+            <div className="col">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Your email"
+                name="emailAddress"
+                id="emailAddress"
+                required
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <textarea
+                className="form-control"
+                placeholder="Your Message"
+                name="message"
+                id="message"
+                cols="30"
+                rows="8"
+                required
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <button class="btn btn-secondary btn-lg" type="submit">
+                Submit form
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
